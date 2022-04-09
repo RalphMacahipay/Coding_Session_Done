828a959eaad52b3a4f9d2e31c3f98efcf506ff05
Ralph Kevin Macahipay <201980122@psu.palawan.edu.ph>
2022-04-09 08:20:55 +0800

    done

---
+++ Edit/sidebar-edit.php
@@ -0,0 +1,32 @@
+<div class="logo">
+                    <a href="javascript:;" class="simple-text">
+                      Your Logo
+                    </a>
+                </div>
+
+
+                <ul class="nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="../transaction.php">
+                        <i class="nc-icon nc-single-copy-04"></i>
+                            <p>Transaction</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="../office.php">
+                        <i class="nc-icon nc-bank"></i>
+                            <p>Office</p>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a class="nav-link" href="../employee.php">
+                        <i class="nc-icon nc-single-02"></i>
+                            <p>Employee</p>
+                        </a>
+                    </li>
+                    
+
+                    
+                </ul>
+
